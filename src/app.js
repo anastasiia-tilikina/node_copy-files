e5f141e7cccbb2a968eb209c1e4f75fbd6c4b9b8
Anastasiia Tilikina <anastasiia.tilikina@bndigital.co>
2023-09-03 13:05:10 +0300

    Create app for coping file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const copyFile = (src, dest) => {
+  const sameDir = path.dirname(src) === dest;
+
+  const fileExt = path.extname(src);
+  const fileName = path.basename(src, fileExt);
+  const copiedFileName = `${fileName}-copy${fileExt}`;
+
+  if (sameDir) {
+    return;
+  }
+
+  try {
+    fs.copyFileSync(src, path.resolve(dest, copiedFileName));
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = { copyFile };
